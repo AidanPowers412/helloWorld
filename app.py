727ab4467512229be0d0b12e14afc0d3172213fa
AidanPowers412 <acpowers@terpmail.umd.edu>
2024-02-20 15:20:26 -0500

    Commiting the app.py file. Created another route. This route is /about.

--- app.py
+++ app.py
@@ -4,12 +4,18 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World from Aidan Powers!I am adding my first code change.'
+def hello_world():  # This function returns a simple string
+    return 'Hello World from Aidan Powers! I am adding my first code change.'
+
 
 @app.route('/hello')
-def hello():  # put application's code here
+def hello():  # This route renders the 'hello.html' template
     return render_template('hello.html')
+
+
+@app.route('/about')
+def about():  # This route renders the 'about.html' template
+    return render_template('about.html')
 
 
 if __name__ == '__main__':

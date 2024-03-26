30dd5c1307fb252296f64d9cf1f2505ce61b250d
AidanPowers412 <acpowers@terpmail.umd.edu>
2024-03-26 17:56:01 -0400

    Created 2 new routes which include /favorite-course and /contact. Created three new HTML files which include favorite-course, contact, and base. These pages are added to the helloWorld flask application and the purpose was to demonstrate adding more pages.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -20,6 +20,24 @@ def about():  # This route renders the 'about.html' template
 @app.route('/about-css')
 def about_css():
     return render_template('about-css.html')
+
+@app.route('/favorite-course')
+def favorite_course():
+    subject = request.args.get('subject', 'No Subject')
+    course_number = request.args.get('course_number', 'No Course Number')
+    return render_template('favorite-course.html', subject=subject, course_number=course_number)
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        firstName = request.form.get('firstName')
+        lastName = request.form.get('lastName')
+        emailAddress = request.form.get('emailAddress')
+        age = request.form.get('age')
+        return render_template('contact.html', submitted=True, firstName=firstName, lastName=lastName, emailAddress=emailAddress, age=age)
+    else:
+        return render_template('contact.html', submitted=False)
+
 
 
 
